2069e17ca633e36438f0e1b91ae7388d2d6aeab0
Patryk <patrykkonior4@gmail.com>
2023-10-25 21:57:38 +0200

    Vite Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom";
+
 const Pet = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
     React.createElement("h2", {}, props.animal),
     React.createElement("h2", {}, props.breed),
-  ])
+  ]);
 };
 
 const App = () => {
-    return React.createElement("div", {}, [
+  return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
       animal: "Dog",
@@ -28,5 +31,5 @@ const App = () => {
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
